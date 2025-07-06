55ed8dadbe0e364071e0b3d5a120d3d89ae1133c
lIlSkaSkaSkalIl <168249736+lIlSkaSkaSkalIl@users.noreply.github.com>
2025-07-06 14:52:49 +0700

    Create labels.py

---
+++ uploader/labels.py
@@ -0,0 +1,18 @@
+# Semua label digunakan dalam status upload
+LABELS = {
+    "file": "*File*",
+    "nama": "*Nama*",
+    "ukuran": "*Ukuran*",
+    "durasi": "*Durasi*",
+    "status": "*Status*",
+    "resolusi": "*Resolusi*",
+    "video": "*Video*",
+    "audio": "*Audio*",
+    "bitrate": "*Bitrate*",
+    "cleanup": "*Cleanup*",
+    "waktu": "*Waktu*",
+    "error": "*Error*",
+}
+
+# Separator standar
+SEP = " »"
