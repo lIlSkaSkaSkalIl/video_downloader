61e5927a6c4642155439e66bfef4a0d792ee9e70
lIlSkaSkaSkalIl <168249736+lIlSkaSkaSkalIl@users.noreply.github.com>
2025-07-09 11:53:25 +0700

    Update google_drive.py

--- downloader/google_drive.py
+++ downloader/google_drive.py
@@ -1,5 +1,11 @@
+# downloader/google_drive.py
+
 import gdown
 import re
+from core.setup_directories import prepare_directories
+import os
+
+dirs = prepare_directories()
 
 def extract_drive_id(url: str) -> str:
     patterns = [
@@ -13,8 +19,14 @@ def extract_drive_id(url: str) -> str:
             return match.group(1)
     return None
 
-def download_from_google_drive(url: str, output_path: str):
+def download_from_google_drive(url: str, filename: str = None) -> str:
     file_id = extract_drive_id(url)
     if not file_id:
         raise ValueError("❌ Tidak dapat mendeteksi ID file dari URL Google Drive.")
+
+    if not filename:
+        filename = f"{file_id}.mp4"
+
+    output_path = os.path.join(dirs["video"], filename)
     gdown.download(f"https://drive.google.com/uc?id={file_id}", output_path, quiet=False)
+    return output_path
