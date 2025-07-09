de979fb5ba16031974d2f7277d3a0011cfeb4c70
lIlSkaSkaSkalIl <168249736+lIlSkaSkaSkalIl@users.noreply.github.com>
2025-07-09 11:54:03 +0700

    Update m3u8.py

--- downloader/m3u8.py
+++ downloader/m3u8.py
@@ -1,10 +1,17 @@
+# downloader/m3u8.py
+
 import subprocess
 import os
+from core.setup_directories import prepare_directories
 
-def download_from_m3u8(url: str, output_path: str):
+dirs = prepare_directories()
+
+def download_from_m3u8(url: str, filename: str = "video_m3u8.mp4") -> str:
     downloader_path = os.path.expanduser("~/.local/bin/downloadm3u8")
     if not os.path.exists(downloader_path):
         raise FileNotFoundError("❌ 'downloadm3u8' tidak ditemukan di ~/.local/bin/. Restart runtime setelah install.")
+
+    output_path = os.path.join(dirs["video"], filename)
 
     cmd = [
         downloader_path,
@@ -15,3 +22,4 @@ def download_from_m3u8(url: str, output_path: str):
     for line in process.stdout:
         print(f"\r{line.strip()[:150]}", end="", flush=True)
     process.wait()
+    return output_path
